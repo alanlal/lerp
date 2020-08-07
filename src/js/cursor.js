be4ad3ad7fc1335f8aaa251c2ef1c9f4ea37e921
Alan Lal <alanlal249@gmail.com>
2020-08-07 18:00:09 +0530

    made lerp based mousemove

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -1,50 +1,49 @@
-import { getMousePos, lerp } from "./utils"
-import gsap from "gsap/gsap-core"
+import { gsap } from 'gsap';
+import { lerp, getMousePos } from './utils';
 
-
-
-let mouse = { x: 0, y: 0 }
-window.addEventListener("mousemove", ev => mouse = getMousePos(ev))
-
+// Track the mouse position
+let mouse = { x: 0, y: 0 };
+window.addEventListener('mousemove', ev => mouse = getMousePos(ev));
 
 export default class Cursor {
     constructor(el) {
-        this.DOM = { el: el }
-        this.bounds = this.DOM.el.getBoundingClientRect()
+        this.DOM = { el: el };
+        this.DOM.el.style.opacity = 0;
+
+        this.bounds = this.DOM.el.getBoundingClientRect();
 
         this.renderStyles = {
-            tx: {
-                current: 0, previous: 0, amt: 0
-            },
-            ty: {
-                current: 0, previous: 0, amt: 0
-            }
-        }
+            tx: { previous: 0, current: 0, amt: 0.2 },
+            ty: { previous: 0, current: 0, amt: 0.2 }
+        };
 
-        this.onMouseMove = () => {
-            this.renderStyles.tx.previous = this.renderStyles.tx.previous = mouse.x - this.bounds.width / 2
-            this.renderStyles.ty.current = this.renderStyles.ty.previous = mouse.y - this.bounds.height / 2
-
-            gsap.to(this.DOM.el, { duration: 0.9, ease: 'Power3.easeOut' })
-
-            requestAnimationFrame(() => this.render())
-            window.removeEventListener('mousemove', this.onMouseMove)
-        }
-
-        window.addEventListener('mousemove', this.onMouseMove.bind(this))
+        this.onMouseMoveEv = () => {
+            gsap.to(this.DOM.el, { duration: 0.9, ease: 'Power3.easeOut', opacity: 1 });
+            requestAnimationFrame(() => this.render());
+            window.removeEventListener('mousemove', this.onMouseMoveEv);
+        };
+        window.addEventListener('mousemove', this.onMouseMoveEv);
     }
-
     render() {
 
         this.renderStyles.tx.current = mouse.x - this.bounds.width / 2
-        this.renderStyles.tx.current = mouse.y - this.bounds.height / 2
+        this.renderStyles.ty.current = mouse.y - this.bounds.height / 2
 
         for (let key in this.renderStyles) {
-            this.renderStyles[key].previous = lerp(this.renderStyles[key].previous, this.renderStyles[key].current, this.renderStyles[key].amt)
+            this.renderStyles[key].previous = lerp(this.renderStyles[key].previous, this.renderStyles[key].current, this.renderStyles[key].amt);
         }
 
+        // this.DOM.el.style.transform = `translateX(${(this.renderStyles['tx'].previous)}px) translateY(${this.renderStyles['ty'].previous}px)`;
         this.DOM.el.style.transform = `translateX(${this.renderStyles.tx.previous}px) translateY(${this.renderStyles.ty.previous}px)`
-        requestAnimationFrame(() => this.render())
+        requestAnimationFrame(() => this.render());
     }
 
+    // render() {
+
+
+    //     for (let key in this.renderStyles) {
+    //     }
+
+    //     requestAnimationFrame(() => this.render())
+    // }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,6 @@ import Cursor from "./cursor";
 
 const cursorElement = document.querySelector(".cursor")
 const cursor = new Cursor(cursorElement)
+
+
+
