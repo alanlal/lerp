eb5576ca82592b49de81e8c9be1debdaa884f0c1
Alan Lal <alanlal249@gmail.com>
2020-08-22 23:13:20 +0530

    testing using the event emitter

--- src/js/domScroller.js
+++ src/js/domScroller.js
@@ -1,38 +1,47 @@
 import { lerp } from "./utils";
-
-
-let docscroll = 0;
-
-window.addEventListener('scroll', function (e) {
-    docscroll = window.pageYOffset || document.documentElement.scrollTop
-})
-
+import eventEmitter from "./eventEmitter";
 
 let body = document.body
 
 export default class DomScroller {
+
     constructor(el) {
         this.DOM = { el: el }
+        this.eventEmitter = eventEmitter
+
+
+        this.docscroll = 0;
+
         this.renderedStyles = {
             positionY: {
                 current: 0,
                 previous: 0,
-                easeAmount: 0.2,
-                setScrollValue: () => docscroll
+                easeAmount: 0.1,
+                setScrollValue: () => this.docscroll
             }
         }
+
         this.setSize()
+        this.eventEmitter.on('RESIZE', () => this.setSize())
+
+
         this.init()
         requestAnimationFrame(() => this.render())
     }
     init() {
+
+        this.eventEmitter.on('SCROLL_POSITION_CHANGED', (docscroll) => {
+            this.docscroll = docscroll
+        })
+
         for (let key in this.renderedStyles) {
             this.renderedStyles[key].current = this.renderedStyles[key].previous = this.renderedStyles[key].setScrollValue()
         }
     }
+
+
     setSize() {
-        body.style.height
-            = `${this.DOM.el.scrollHeight + 100}px`
+        body.style.height = `${this.DOM.el.scrollHeight + 100}px`
     }
     render() {
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,19 @@
 import Cursor from "./cursor";
 import DOMScroller from "./domScroller"
+import eventEmitter from "./eventEmitter";
 
 const cursorElement = document.querySelector(".cursor")
 const boxContainer = document.querySelector(".box-container")
+
+window.addEventListener('resize', () => {
+    eventEmitter.emit('RESIZE')
+})
+
+window.addEventListener('scroll', () => {
+    let docscroll = window.pageYOffset || document.documentElement.scrollTop
+    eventEmitter.emit('SCROLL_POSITION_CHANGED', docscroll)
+})
+
 
 const cursor = new Cursor(cursorElement)
 const domScroller = new DOMScroller(boxContainer)
